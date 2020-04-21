5ffcb1a20f11566f0f5b060aefe644821a3921eb
GhostX <ghostx@localhost.localdomain>
2020-04-20 21:09:33 -0800

    Methods added

--- Program.cs
+++ Program.cs
@@ -6,9 +6,26 @@ namespace dz20._04
 {
     class Program
     {
+        delegate T akmal<T>(T x, T y);
         static void Main(string[] args)
         {
             
-        }        
+        }
+        static T mul<T>(T x, T y)
+        {
+            return (dynamic)x * (dynamic)y;
+        }
+        static T div<T>(T x, T y)
+        {
+            return (dynamic)x / (dynamic)y;
+        }
+        static T sum<T>(T x, T y)
+        {
+            return (dynamic)x + (dynamic)y;
+        }
+        static T min<T>(T x, T y)
+        {
+            return (dynamic)x - (dynamic)y;
+        }
     }
 }
