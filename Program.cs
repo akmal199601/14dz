04f22919f91c6fcf9c5fc26b17f154b26d516c80
GhostX <ghostx@localhost.localdomain>
2020-04-20 21:09:09 -0800

    project inited

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace dz20._04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }        
+    }
+}
